57e44d8df66bcfb039eaf9f120845ec37359efe4
jeremiedubuis <jeremie.dubuis@gmail.com>
2019-06-04 17:02:23 +0200

    :rotating_light: Fix indents

--- example/example.jsx
+++ example/example.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
-import GenericForm, { GenericFormFieldDataTypes, GenericFormFieldTypes } from '../src/index';
+import {render} from 'react-dom';
+import GenericForm, {GenericFormFieldDataTypes, GenericFormFieldTypes} from '../src/index';
 import GenericFormField from '../src/GenericFormField';
 
 class App extends React.Component {
@@ -11,205 +11,205 @@ class App extends React.Component {
   }
 
   render() {
-      return <div>
+    return <div>
 
-          <h1> Test 1</h1>
-          <GenericForm id="test"
-                       onSubmit={(e, vals) => {
-                           e.preventDefault();
-                           console.log(vals);
-                       }}
-                       onError={(err,f) => console.log('Error', err, f) }
-                       fields={[
-              {
-                  type: GenericFormFieldTypes.TEXT,
-                  name: 'firstname',
-                  id: 'test-firstname-field',
-                  label: 'Firstname',
-                  validation: {
-                      mandatory: true,
-                      errorEmpty: 'Firstname is mandatory'
-                  }
-              },
-              {
-                  type: GenericFormFieldTypes.TEXT,
-                  name: 'lastname',
-                  id: 'test-lastname-field',
-                  label: 'Lastname',
-                  validation: {
-                      mandatory: true
-                  }
-              },
-              {
-                  type: GenericFormFieldTypes.SELECT,
-                  name: 'title',
-                  id: 'test-title-field',
-                  label: 'Title',
-                  options: [
-                      {
-                          label: 'Pick',
-                          value: ''
-                      },
-                      {
-                          label: 'Ms',
-                          value: 'Ms'
-                      },
-                      {
-                          label: 'Mr',
-                          value: 'Mr'
-                      },
-                  ],
-                  validation: {
-                      mandatory: true
-                  }
-              },
-              {
-                  type: GenericFormFieldTypes.CHECKBOX,
-                  name: 'terms',
-                  id: 'test-terms-field',
-                  value:'accept',
-                  label: 'Accept terms and conditions',
-                  validation: {
-                      mandatory: true
-                  },
-                  dataType: GenericFormFieldDataTypes.BOOL
-              }
-          ]}>
-              <p>Optional checkboxes</p>
-              <GenericFormField
-                  formId="test"
-                  type={GenericFormFieldTypes.CHECKBOX}
-                  id="test-c1-field"
-                  label="C1"
-                  value={"c1"}
-                  name="c1" />
-              <GenericFormField
-                  formId="test"
-                  type={GenericFormFieldTypes.CHECKBOX}
-                  id="test-c2-field"
-                  label="C2"
-                  name="c2" />
-              <p>Field with forbidden value ','</p>
-              <GenericFormField
-                  formId="test"
-                  type={GenericFormFieldTypes.TEXT}
-                  id="test-address-field" name={"address"}
-                  label="Address"
-                  validation={ {
-                      mandatory: true,
-                      positiveRegex: {
-                          'Commas are not allowed': ','
-                      }
-                  } } />
-              <p>Field with mandatory comma</p>
-              <GenericFormField
-                  formId="test"
-                  type={GenericFormFieldTypes.TEXT}
-                  id="test-address-field" name={"address"}
-                  label="Address"
-                  validation={ {
-                      mandatory: true,
-                      negativeRegex: {
-                          'Field must contain a comma': ','
-                      }
-                  } } />
-              <p>Field with customErrorHandler</p>
-              <GenericFormField
-                  formId="test"
-                  type={GenericFormFieldTypes.TEXT}
-                  id="test-bad-valuefield" name={"bad-value"}
-                  label="Do not enter 'Bad value'"
-                  validation={ {
-                      mandatory: true,
-                      customErrorHandlers: {
-                          'Do not enter "Bad value"':  v => v === 'Bad value'
-                      }
-                  } } />
-              <GenericFormField
-                  formId="test"
-                  type={GenericFormFieldTypes.SUBMIT}
-                  value="Send"
-              />
-          </GenericForm>
+      <h1> Test 1</h1>
+      <GenericForm id="test"
+                   onSubmit={(e, vals) => {
+                     e.preventDefault();
+                     console.log(vals);
+                   }}
+                   onError={(err, f) => console.log('Error', err, f)}
+                   fields={[
+                     {
+                       type: GenericFormFieldTypes.TEXT,
+                       name: 'firstname',
+                       id: 'test-firstname-field',
+                       label: 'Firstname',
+                       validation: {
+                         mandatory: true,
+                         errorEmpty: 'Firstname is mandatory'
+                       }
+                     },
+                     {
+                       type: GenericFormFieldTypes.TEXT,
+                       name: 'lastname',
+                       id: 'test-lastname-field',
+                       label: 'Lastname',
+                       validation: {
+                         mandatory: true
+                       }
+                     },
+                     {
+                       type: GenericFormFieldTypes.SELECT,
+                       name: 'title',
+                       id: 'test-title-field',
+                       label: 'Title',
+                       options: [
+                         {
+                           label: 'Pick',
+                           value: ''
+                         },
+                         {
+                           label: 'Ms',
+                           value: 'Ms'
+                         },
+                         {
+                           label: 'Mr',
+                           value: 'Mr'
+                         },
+                       ],
+                       validation: {
+                         mandatory: true
+                       }
+                     },
+                     {
+                       type: GenericFormFieldTypes.CHECKBOX,
+                       name: 'terms',
+                       id: 'test-terms-field',
+                       value: 'accept',
+                       label: 'Accept terms and conditions',
+                       validation: {
+                         mandatory: true
+                       },
+                       dataType: GenericFormFieldDataTypes.BOOL
+                     }
+                   ]}>
+        <p>Optional checkboxes</p>
+        <GenericFormField
+          formId="test"
+          type={GenericFormFieldTypes.CHECKBOX}
+          id="test-c1-field"
+          label="C1"
+          value={"c1"}
+          name="c1"/>
+        <GenericFormField
+          formId="test"
+          type={GenericFormFieldTypes.CHECKBOX}
+          id="test-c2-field"
+          label="C2"
+          name="c2"/>
+        <p>Field with forbidden value ','</p>
+        <GenericFormField
+          formId="test"
+          type={GenericFormFieldTypes.TEXT}
+          id="test-address-field" name={"address"}
+          label="Address"
+          validation={{
+            mandatory: true,
+            positiveRegex: {
+              'Commas are not allowed': ','
+            }
+          }}/>
+        <p>Field with mandatory comma</p>
+        <GenericFormField
+          formId="test"
+          type={GenericFormFieldTypes.TEXT}
+          id="test-address-field" name={"address"}
+          label="Address"
+          validation={{
+            mandatory: true,
+            negativeRegex: {
+              'Field must contain a comma': ','
+            }
+          }}/>
+        <p>Field with customErrorHandler</p>
+        <GenericFormField
+          formId="test"
+          type={GenericFormFieldTypes.TEXT}
+          id="test-bad-valuefield" name={"bad-value"}
+          label="Do not enter 'Bad value'"
+          validation={{
+            mandatory: true,
+            customErrorHandlers: {
+              'Do not enter "Bad value"': v => v === 'Bad value'
+            }
+          }}/>
+        <GenericFormField
+          formId="test"
+          type={GenericFormFieldTypes.SUBMIT}
+          value="Send"
+        />
+      </GenericForm>
 
-          <h1>Test 2: Submit is disabled until the form is valid</h1>
-          <GenericForm id="test2"
-              ref={this.form2Ref}
-              onSubmit={(e, vals) => {
-                  e.preventDefault();
-                  console.log(vals);
-              }}>
-              <p>Group with at least one mandatory checkbox</p>
-              <GenericFormField
-                  formId="test2"
-                  type={GenericFormFieldTypes.CHECKBOX}
-                  id="test-checkbox-field2"
-                  name={"checkbox"}
-                  value={"value1"}
-                  label="Checkbox 1"
-                  validation={ {
-                      group:'checkbox-group',
-                      errorGroup: 'You must at least check one checkbox'
-                  } } />
-              <GenericFormField
-                  formId="test2"
-                  type={GenericFormFieldTypes.CHECKBOX}
-                  id="test-checkbox-field2"
-                  name={"checkbox"}
-                  value={"value2"}
-                  label="Checkbox 2"
-                  validation={ {
-                      group:'checkbox-group'
-                  } } />
-              <GenericFormField
-                  formId="test2"
-                  type={GenericFormFieldTypes.PASSWORD}
-                  id="test-checkbox-field3"
-                  name={"password1"}
-                  label="Password 1"
-                  validation={ {
-                      identicalGroup: 'password-group',
-                      errorIdenticalGroup: 'Both passwords must be identical'
-                  } } />
-              <GenericFormField
-                  formId="test2"
-                  type={GenericFormFieldTypes.PASSWORD}
-                  id="test-checkbox-field4"
-                  name={"password2"}
-                  label="Password 2"
-                  validation={ {
-                      identicalGroup: 'password-group',
-                      errorIdenticalGroup: 'Both passwords must be identical'
-                  } } />
+      <h1>Test 2: Submit is disabled until the form is valid</h1>
+      <GenericForm id="test2"
+                   ref={this.form2Ref}
+                   onSubmit={(e, vals) => {
+                     e.preventDefault();
+                     console.log(vals);
+                   }}>
+        <p>Group with at least one mandatory checkbox</p>
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.CHECKBOX}
+          id="test-checkbox-field2"
+          name={"checkbox"}
+          value={"value1"}
+          label="Checkbox 1"
+          validation={{
+            group: 'checkbox-group',
+            errorGroup: 'You must at least check one checkbox'
+          }}/>
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.CHECKBOX}
+          id="test-checkbox-field2"
+          name={"checkbox"}
+          value={"value2"}
+          label="Checkbox 2"
+          validation={{
+            group: 'checkbox-group'
+          }}/>
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.PASSWORD}
+          id="test-checkbox-field3"
+          name={"password1"}
+          label="Password 1"
+          validation={{
+            identicalGroup: 'password-group',
+            errorIdenticalGroup: 'Both passwords must be identical'
+          }}/>
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.PASSWORD}
+          id="test-checkbox-field4"
+          name={"password2"}
+          label="Password 2"
+          validation={{
+            identicalGroup: 'password-group',
+            errorIdenticalGroup: 'Both passwords must be identical'
+          }}/>
 
-              <GenericFormField
-                formId="test2"
-                type={GenericFormFieldTypes.RADIO}
-                id="test-radio-field"
-                name={"radio"}
-                value={"value1"}
-                label="Radio 1" />
-              <GenericFormField
-                formId="test2"
-                type={GenericFormFieldTypes.RADIO}
-                id="test-radio-field2"
-                name={"radio"}
-                value={"value2"}
-                label="Radio 2" />
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.RADIO}
+          id="test-radio-field"
+          name={"radio"}
+          value={"value1"}
+          label="Radio 1"/>
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.RADIO}
+          id="test-radio-field2"
+          name={"radio"}
+          value={"value2"}
+          label="Radio 2"/>
 
-              <GenericFormField
-                  formId="test2"
-                  type={GenericFormFieldTypes.SUBMIT}
-                  disableUntilValid
-                  value="Send"
-              />
-          </GenericForm>
+        <GenericFormField
+          formId="test2"
+          type={GenericFormFieldTypes.SUBMIT}
+          disableUntilValid
+          value="Send"
+        />
+      </GenericForm>
 
-        <button type="button" onClick={() => this.form2Ref.current.reset() }>Reset</button>
+      <button type="button" onClick={() => this.form2Ref.current.reset()}>Reset</button>
 
-      </div>;
+    </div>;
 
   }
 }
 
-render(<App />, document.getElementById('app'));
+render(<App/>, document.getElementById('app'));
--- src/GenericForm.jsx
+++ src/GenericForm.jsx
@@ -1,88 +1,89 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import GenericFormField  from './GenericFormField';
-import { isRadioOrCheckbox } from './helpers';
+import GenericFormField from './GenericFormField';
+import {isRadioOrCheckbox} from './helpers';
 
 class GenericForm extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.fields = [];
-        this._onSubmit = this._onSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.fields = [];
+    this._onSubmit = this._onSubmit.bind(this);
+  }
 
-    render() {
-        return <form id={this.props.id} onSubmit={ this._onSubmit } className={ 'generic-form '+(this.props.className ? this.props.className: '') }>
-            { this.content() }
-            { this.props.children }
-        </form>;
-    }
+  render() {
+    return <form id={this.props.id} onSubmit={this._onSubmit}
+                 className={'generic-form ' + (this.props.className ? this.props.className : '')}>
+      {this.content()}
+      {this.props.children}
+    </form>;
+  }
 
-    content() {
-        return (this.props.fields || []).map((f, i) => f ?
-            <GenericFormField formId={this.props.id} key={i} {...f} /> : null);
-    }
+  content() {
+    return (this.props.fields || []).map((f, i) => f ?
+      <GenericFormField formId={this.props.id} key={i} {...f} /> : null);
+  }
 
-    _onSubmit(e) {
-        const fields = GenericFormField.getFields(this.props.id);
-        if (fields.length) {
-            const isValid = fields[0].validateFields();
-            if (!isValid) {
-                e.preventDefault();
-                return;
-            }
+  _onSubmit(e) {
+    const fields = GenericFormField.getFields(this.props.id);
+    if (fields.length) {
+      const isValid = fields[0].validateFields();
+      if (!isValid) {
+        e.preventDefault();
+        return;
+      }
+    }
+    if (typeof this.props.onSubmit === 'function')
+      this.props.onSubmit(e, this.getValues());
+
+  }
+
+  getValues() {
+    let checkboxes = {};
+    const values = GenericFormField.getFields(this.props.id).reduce((acc, f) => {
+      let val = f.getValue();
+      if (!isRadioOrCheckbox(f.props)) {
+        if (val !== '' && val !== null) acc[f.props.name] = f.getValue();
+      } else {
+        if (!checkboxes[f.props.name]) {
+          checkboxes[f.props.name] = [];
         }
-        if (typeof this.props.onSubmit === 'function')
-            this.props.onSubmit(e, this.getValues());
+        checkboxes[f.props.name].push(val);
+      }
 
+      return acc;
+    }, {});
+
+    for (let key in checkboxes) {
+      checkboxes[key] = checkboxes[key].filter(val => !!val);
+      if (!checkboxes[key].length) checkboxes[key] = false;
+      else if (!key.endsWith('[]') && checkboxes[key].length === 1) {
+        checkboxes[key] = checkboxes[key][0];
+      }
     }
 
-    getValues() {
-        let checkboxes = {};
-        const values = GenericFormField.getFields(this.props.id).reduce((acc, f) => {
-            let val = f.getValue();
-            if (!isRadioOrCheckbox(f.props)) {
-                if (val !== '' && val !== null) acc[f.props.name] = f.getValue();
-            } else {
-                if(!checkboxes[f.props.name]) {
-                    checkboxes[f.props.name] = [];
-                }
-                checkboxes[f.props.name].push(val);
-            }
-
-            return acc;
-        }, {});
-
-        for(let key in checkboxes) {
-            checkboxes[key] = checkboxes[key].filter(val => !!val);
-            if (!checkboxes[key].length) checkboxes[key] = false;
-            else if (!key.endsWith('[]') && checkboxes[key].length === 1) {
-                checkboxes[key] = checkboxes[key][0];
-            }
-        }
-
-        for(let key in checkboxes) {
-            if (key.endsWith('[]')) {
-                checkboxes[key.replace('[]','')] = checkboxes[key];
-                delete checkboxes[key];
-            }
-        }
-
-        return { ...values, ...checkboxes };
+    for (let key in checkboxes) {
+      if (key.endsWith('[]')) {
+        checkboxes[key.replace('[]', '')] = checkboxes[key];
+        delete checkboxes[key];
+      }
     }
 
-    reset() {
-        GenericFormField.getFields(this.props.id).forEach(f => f.reset());
-    }
+    return {...values, ...checkboxes};
+  }
+
+  reset() {
+    GenericFormField.getFields(this.props.id).forEach(f => f.reset());
+  }
 
 }
 
 GenericForm.propTypes = {
-    id: PropTypes.string,
-    onError: PropTypes.func,
-    onSubmit: PropTypes.func,
-    children: PropTypes.node,
-    className: PropTypes.string,
+  id: PropTypes.string,
+  onError: PropTypes.func,
+  onSubmit: PropTypes.func,
+  children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default GenericForm;
